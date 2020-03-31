69eb4af0c713673e570f6768d30e543c7c5efc8a
talgoldfryd <talgold9@gmail.com>
2020-03-31 17:24:31 +0300

    Icrease number of word every N-epoch

--- FinalProject/Code/VideoDatasSet.py
+++ FinalProject/Code/VideoDatasSet.py
@@ -3,6 +3,7 @@ import torchvision
 import pandas as pd
 import os
 import glob
+from math import ceil
 import torch.utils.data as data
 
 
@@ -18,18 +19,34 @@ def collate_fn(batch):
 
 
 class VGGDataSet(torch.utils.data.Dataset):
-    def __init__(self, root_dir, subtitles_dir, video_crop_size=100, transform=None):
+    def __init__(self, root_dir, subtitles_dir, increase_one_word_every_N_epochs=2, video_crop_size=100, transform=None,
+                 crop_lips=False):
         self.data_path = root_dir
         self.subtitles_dir = subtitles_dir
-        videos_and_subtitles = self.get_videos_and_subtitles()
+        if crop_lips:
+            videos_and_subtitles = self.get_videos_and_subtitles_with_lip_crods()
+        else:
+            videos_and_subtitles = self.get_videos_and_subtitles()
         self.metadata = pd.DataFrame({'VideoFile': videos_and_subtitles[0], 'SubtitleFile': videos_and_subtitles[1]})
         self.video_crop_size = video_crop_size
         self.transform = transform
+        self.sep_line = ' \t' if crop_lips else ' '
+        self.increase_one_word_every_N_epochs = increase_one_word_every_N_epochs
+        self.increase_one_word_every_N_iters = self.increase_one_word_every_N_epochs * self.__len__()
+        self.counter_iterations = 0
+        self.num_of_words = 0
+
 
     def __len__(self):
         return len(self.metadata)
 
     def __getitem__(self, idx):
+
+        if not(self.counter_iterations % self.increase_one_word_every_N_iters):
+            self.num_of_words += 1
+
+        self.counter_iterations += 1
+
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
@@ -38,8 +55,11 @@ class VGGDataSet(torch.utils.data.Dataset):
 
         video_obj = torchvision.io.read_video(video_path, start_pts=0, end_pts=None, pts_unit='pts')
         video = video_obj[0]
+        sound = video_obj[1]
         video_metadata = video_obj[2]
-        subtitle = self.read_txt(subtitle_path)
+        subtitle = self.read_txt(subtitle_path, self.sep_line)
+
+        video, subtitle = self.create_video_using_num_of_words(video, sound, video_metadata, subtitle)
 
         # Crop video in center
         video = self.crop_center(video, self.video_crop_size, self.video_crop_size)
@@ -54,7 +74,7 @@ class VGGDataSet(torch.utils.data.Dataset):
 
         return sample
 
-    def get_videos_and_subtitles(self):
+    def get_videos_and_subtitles_with_lip_crods(self):
         mp4_list = []
         txt_list = []
         all_dirs = os.listdir(self.data_path)
@@ -70,8 +90,41 @@ class VGGDataSet(torch.utils.data.Dataset):
 
         return [mp4_list, txt_list]
 
+    def get_videos_and_subtitles(self):
+        mp4_list = []
+        txt_list = []
+        all_dirs = os.listdir(self.data_path)
+
+        for _dir in all_dirs:
+            mp4_in_dir = glob.glob('{}/{}/*.mp4'.format(self.data_path, _dir))
+            txt_in_dir = [mp4.split('.mp4')[0] + '.txt' for mp4 in mp4_in_dir]
+
+            mp4_list = mp4_list + mp4_in_dir
+            txt_list = txt_list + txt_in_dir
+
+        return [mp4_list, txt_list]
+
+    def create_video_using_num_of_words(self, video, sound, video_metadata, subtitle):
+        if self.num_of_words > subtitle['Text'].split(' ').__len__():
+            num_of_words = subtitle['Text'].__len__()
+        else:
+            num_of_words = self.num_of_words
+        subtitle['Text'] = ' '.join(subtitle['Text'].split(' ')[:num_of_words])
+
+        time_for_each_frame = 1 / video_metadata['video_fps']
+        subtitle_timing_df = subtitle['frame_info']
+        subtitle_timing_for_current_word_timing = float(subtitle_timing_df.iloc[num_of_words]['END'])
+        n_frames = int(ceil(subtitle_timing_for_current_word_timing / time_for_each_frame))
+        video = video[:n_frames]
+
+        time_for_each_frame_sound = 1 / video_metadata['audio_fps']
+        n_frames_sound = int(ceil(time_for_each_frame_sound / time_for_each_frame))
+        sound = sound[:, :n_frames_sound]
+
+        return video, sound, subtitle
+
     @staticmethod
-    def read_txt(sub_file):
+    def read_txt(sub_file, sep_line):
         y = {}
         frames_info = []
         flag = True
@@ -85,7 +138,7 @@ class VGGDataSet(torch.utils.data.Dataset):
                     value = value[2:]
                     y[key] = value
                 else:
-                    frames_info.append(line.replace(' \n', '').split(' \t'))
+                    frames_info.append(line.replace(' \n', '').split(sep_line))
         frames_info_df = pd.DataFrame(data=frames_info[1:], columns=frames_info[0])
         y['frame_info'] = frames_info_df
         return y
